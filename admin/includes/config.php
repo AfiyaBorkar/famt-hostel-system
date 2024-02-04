c6354f708b089c6defa7e13f802f8327b371aa08
Afiya <afiyaborkar34@gmail.com>
2024-02-04 22:20:32 +0530

    final commit

---
+++ admin/includes/config.php
@@ -0,0 +1,40 @@
+
+<?php
+// $dbuser="id20232029_famthosteldemo";
+// $dbpass="Saif@12345678";
+// $host="localhost";
+// $db="id20232029_famt";
+
+
+
+// $host="localhost";
+// $DB_host =$host;
+
+// $dbuser="root";
+// $DB_user=$dbuser;
+
+// $dbpass="";
+// $DB_pass=$dbpass;
+
+// $db="famt_hostel";
+// $DB_name=$db;
+
+
+
+// $dbuser="id20232029_famthosteldemo";
+// $dbpass="Saif@12345678";
+// $host="localhost";
+// $db="id20232029_famt";
+// $DB_host =$host;
+// $DB_user=$dbuser;
+// $DB_pass=$dbpass;
+// $DB_name=$db;
+
+// $mysqli =new mysqli($host,$dbuser, $dbpass, $db);
+
+
+include('../includes/config.php');
+
+
+?>
+
